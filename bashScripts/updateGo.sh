cc0cf047e85194cd25d82f322d16ff9395d7b5f9
vb1980 <17623205@qq.com>
2021-02-03 21:36:19 +0800

    Update updateGo.sh

--- bashScripts/updateGo.sh
+++ bashScripts/updateGo.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#rm -rf /opt/hostedtoolcache/go/1.14.14
-#mv /opt/hostedtoolcache/go/1.15.7 /opt/hostedtoolcache/go/1.14.14
 goCacheRoot="/opt/hostedtoolcache/go"
 goVersions="/opt/GoVersions.txt"
 :> $goVersions
