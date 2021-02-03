82f5cdb09dc97df0be6771cf3fbb7f113a610d71
vb1980 <17623205@qq.com>
2021-02-03 21:25:59 +0800

    Update updateGo.sh

--- bashScripts/updateGo.sh
+++ bashScripts/updateGo.sh
@@ -8,12 +8,12 @@ chmod 777 $goVersions
 goCacheDirs=$(ls -l "$goCacheRoot" | sed '{1!G;h;$!d}' | awk '/^d/ {print $NF}')
 for goVersion in $goCacheDirs
 do
-#echo $goVersion
+echo $goVersion
 echo $goVersion >> $goVersions
 done
 new_ver=`sed -n '1p' $goVersions`
 old_ver=`sed -n '2p' $goVersions`
-#echo "rm -rf $goCacheRoot/$old_ver"
+echo "rm -rf $goCacheRoot/$old_ver"
 rm -rf $goCacheRoot/$old_ver
-#echo "mv $goCacheRoot/$new_ver $goCacheRoot/$old_ver"
+echo "mv $goCacheRoot/$new_ver $goCacheRoot/$old_ver"
 mv $goCacheRoot/$new_ver $goCacheRoot/$old_ver
