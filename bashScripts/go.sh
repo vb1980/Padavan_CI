8f17898598ce6cd3ed660d38de5d4ab8ae9d86c4
vb1980 <17623205@qq.com>
2021-02-04 12:22:53 +0800

    Create go.sh

---
+++ bashScripts/go.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+goCacheRootDir="/opt/hostedtoolcache/go"
+goVersions="/opt/goVersions.txt"
+:> $goVersions
+chmod 777 $goVersions
+dir=$(ls -l "$goCacheRootDir" | sed '{1!G;h;$!d}' | awk '/^d/ {print $NF}')
+for i in $dir
+do
+echo $i
+echo $i >> $goVersions
+done
+new_ver=`sed -n '1p' $goVersions`
+old_ver=`sed -n '2p' $goVersions`
+echo "rm -rf $goCacheRootDir/$old_ver"
+rm -rf $goCacheRootDir/$old_ver
+echo "mv $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver"
+mv $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver
