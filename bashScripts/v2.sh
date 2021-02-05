6efee7115eb273347bf731b3efd04a2b09465c86
vb1980 <vb1980@sina.com>
2021-02-05 20:40:19 +0800

    update v2.sh

--- bashScripts/v2.sh
+++ bashScripts/v2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-v2PkgVer=4.34.0
+v2PkgVer="4.34.0"
 v2PkgPath=$1
 echo $v2PkgPath
 echo $v2PkgVer
 cp -f v2ray/v2ray-core-$v2PkgVer.tar.gz $v2PkgPath
-sed -i "s#V2ray_VERSION := 4.31.2#V2ray_VERSION := $v2PkgVer#g" $v2PkgPath/Makefile
-#sed -i "s#V2ray_URL := https://codeload.github.com/v2ray/v2ray-core/tar.gz/v\$(V2ray_VERSION)#V2ray_URL := https://codeload.github.com/v2fly/v2ray-core/tar.gz/v\$(V2ray_VERSION)#g" $v2PkgPath/Makefile
+sed -i "s#V2ray_VERSION := 4.31.2#V2ray_VERSION := $v2PkgVer#g" $(v2PkgPath)/Makefile
+#sed -i "s#V2ray_URL := https://codeload.github.com/v2ray/v2ray-core/tar.gz/v\$(V2ray_VERSION)#V2ray_URL := https://codeload.github.com/v2fly/v2ray-core/tar.gz/v\$(V2ray_VERSION)#g" $(v2PkgPath)/Makefile
