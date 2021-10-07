1fe4f20a4116527d7faa2c309b2793bd53545b27
vb1980 <17623205@qq.com>
2021-10-07 14:59:59 +0800

    Update go.sh

--- bashScripts/go.sh
+++ bashScripts/go.sh
@@ -10,7 +10,7 @@ echo $i
 echo $i >> $goVersions
 done
 new_ver=`sed -n '1p' $goVersions`
-old_ver=`sed -n '2p' $goVersions`
+old_ver=`sed -n '3p' $goVersions`
 echo "rm -rf $goCacheRootDir/$old_ver"
 rm -rf $goCacheRootDir/$old_ver
 #echo "mv $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver"
